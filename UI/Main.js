416ad67404c2e0d9d819939d2dc314f97ba47433
Max <maksimhomisak64@gmail.com>
2021-11-03 23:21:49 +0200

    Error modal
    added error modal-dialog

--- UI/Main.js
+++ UI/Main.js
@@ -1,10 +1,44 @@
-let btn = document.querySelector("#btn");
+let connectionBtn = document.querySelector("#btn");
 
-btn.addEventListener("click", sendAPIHash);
+connectionBtn.addEventListener("click", sendAPIHash);
 
 function sendAPIHash(){
-    let APIHash = document.querySelector("#APIHash").value;
-    if(APIHash != false){
+    let APIHash = document.getElementById('APIHash').value;
+
+    APIHash = APIHash.split(' ').join("");
+    console.log(APIHash);
+
+    if(APIHash != ""){
         $('#Code_Asking').modal();
+    }else{
+        showError("empty");
+    }
+}
+
+function showError(error){
+    if(error == "empty"){
+        document.getElementById("Error_pass_text").innerHTML = "Input is Empty";
+    }else if(error == "code"){
+        document.getElementById("Error_pass_text").innerHTML = "Code is not valid";
+    }
+    $('#Error_pass').modal();
+}
+
+let codeBtn = document.querySelector("#Code_From_Watch_btn");
+
+codeBtn.addEventListener("click", connection);
+
+function connection(){
+    let code = document.getElementById('Code_From_Watch_Input').value;
+
+    let rightCode = "max";
+
+    if(code == rightCode){
+        //Going to Main Page
+    }else{
+        $(function () {
+            $('#Code_Asking').modal('toggle');
+        });
+       showError("code");
     }
 }
