7accbc01423f8eda77bc55a0ce6ab80f4e79085f
Max <maksimhomisak64@gmail.com>
2021-11-21 21:09:03 +0200

    UI
    Part of Display Info

--- UI/Main.js
+++ UI/Main.js
@@ -1,6 +1,9 @@
 let connectionBtn = document.querySelector("#btn");
 
-connectionBtn.addEventListener("click", sendAPIHash);
+if(connectionBtn){
+    connectionBtn.addEventListener("click", sendAPIHash);
+}
+
 
 function sendAPIHash(){
     let APIHash = document.getElementById('APIHash').value;
@@ -25,12 +28,15 @@ function showError(error){
 
 let codeBtn = document.querySelector("#Code_From_Watch_btn");
 
-codeBtn.addEventListener("click", connection);
+if(codeBtn){
+    codeBtn.addEventListener("click", connection);
+}
+
 
 function connection(){
     let code = document.getElementById('Code_From_Watch_Input').value;
 
-    let rightCode = "max";
+    let rightCode = "max"; // test
 
     if(code == rightCode){
         window.location.replace("Main.html");
@@ -50,3 +56,89 @@ function hideAPIHash() {
       x.type = "password";
     }
   }
+
+// Tokens from coinCapAPI in select - Start
+select = document.getElementById('Tokens_Selector');
+
+const requestURL = 'https://api.coincap.io/v2/assets';
+
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', requestURL);
+
+let priceArray = [];
+let namesArray = [];
+
+xhr.onload = () => {
+    for(let i = 0; i < 20; i++){
+        let token = document.createElement('option');
+        let tryPrice = (JSON.parse(xhr.response).data[i].priceUsd);
+        namesArray[i] = (JSON.parse(xhr.response).data[i].symbol);
+        token.innerHTML = (JSON.parse(xhr.response).data[i].name);
+        token.value = namesArray[i];
+        if(namesArray[i] == 'SHIB'){
+            priceArray[i] = ((+tryPrice)*1000).toFixed(3);
+        }else{
+            priceArray[i] = (+tryPrice).toFixed(2);
+        }
+
+        select.appendChild(token);
+    } 
+    
+}
+
+xhr.send();
+// Tokens from coinCapAPI in select - END
+
+
+//Show_Hide options in DisplayInfo - Start
+
+function showOptions(id){
+    let w = document.getElementById(id);
+    
+    if(w.style.display == 'inline'){
+        w.style.display = 'none';
+        document.getElementById("additional_info").style.display = 'none';
+    }else{
+        w.style.display = 'inline';
+        document.getElementById("additional_info").style.display = 'inline';
+    }
+
+}
+//Show_Hide options in DisplayInfo - END
+
+//Show edition info about current crypto - Start
+function showCrypto(){
+    document.getElementById("additional_info").innerHTML = "";
+    let symbol  =   document.getElementById("Tokens_Selector").value;
+    let name    =   document.getElementById("Tokens_Selector");
+    name        =   ((name.options[name.selectedIndex].text).toLowerCase()).split(" ").join("-");
+    
+    let num = namesArray.indexOf(symbol);
+    symbol = symbol.toLowerCase();
+    
+    let price = document.createElement("H1");
+    let logo = document.createElement("IMG");
+
+    let priceText   =   document.createTextNode(priceArray[num] + "$");
+
+    
+
+    price.appendChild(priceText);
+    price.setAttribute("id", "cryptoPrice");
+    logo.setAttribute("src", `https://cryptologos.cc/logos/${name}-${symbol}-logo.png?v=014`);
+    logo.setAttribute("width", "40");
+    logo.setAttribute("height", "40");
+    logo.setAttribute("id", "cryptoLogo");
+    logo.setAttribute("alt", "Token Logo");
+    document.getElementById("additional_info").appendChild(logo); 
+    document.getElementById("additional_info").appendChild(price);
+
+    document.getElementById("cryptoLogo").style.display = 'inline';
+    document.getElementById("cryptoPrice").style.display = 'inline';
+    document.getElementById("cryptoPrice").style.marginLeft = '20px';
+    document.getElementById("cryptoPrice").style.marginTop = '5px';
+    document.getElementById("cryptoPrice").style.fontSize = '20px';
+
+}
+//Show edition info about current crypto - END
